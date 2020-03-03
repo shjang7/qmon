8825284000548e76764b076d4c5c4b6dfb3dcd0c
shjang7 <suhy.jang.shj@gmail.com>
2020-03-03 14:05:55 +0900

    Add nested form for product_items

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,9 +16,18 @@ document.addEventListener("turbolinks:load", () => {
   $('[data-toggle="popover"]').popover()
 })
 
+// import { Application } from "stimulus"
+// import { definitionsFromContext } from "stimulus/webpack-helpers"
+//
+// const application = Application.start()
+// const context = require.context("../controllers", true, /\.js$/)
+// application.load(definitionsFromContext(context))
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import "controllers"
