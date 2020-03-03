600782297be1cf4be44026244d9b75ca1c9f6b20
shjang7 <lucy.sh.jang@gmail.com>
2020-03-03 14:17:03 +0900

    Add image column into product_item

--- app/models/product_item.rb
+++ app/models/product_item.rb
@@ -1,4 +1,8 @@
 class ProductItem < ApplicationRecord
-  validates :title, :price, :total_amount, presence: true
   belongs_to :product, inverse_of: :product_items
+
+  has_one_attached :image, dependent: :destroy
+  mount_uploader :image, ImageUploader
+
+  validates :title, :price, :total_amount, :image, presence: true
 end
