066654607be22b922d0f9b80d3a07bffec62e4bd
shjang7 <suhy.jang.shj@gmail.com>
2020-03-03 14:05:55 +0900

    Add nested form for product_items

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
-  has_many :product_items, inverse_of: :product, dependent: :destroy
   belongs_to :category, inverse_of: :products
+  has_many :product_items, inverse_of: :product, dependent: :destroy
+  accepts_nested_attributes_for :product_items, reject_if: :all_blank, allow_destroy: true
 
   has_one_attached :image, dependent: :destroy
   mount_uploader :image, ImageUploader
--- app/models/product_item.rb
+++ app/models/product_item.rb
@@ -1,7 +1,4 @@
 class ProductItem < ApplicationRecord
-  validates :title, presence: true
-  validates :total_amount, presence: true
-  validates :buy_amount, presence: true
-  validates :price, presence: true
+  validates :title, :price, :total_amount, presence: true
   belongs_to :product, inverse_of: :product_items
 end
