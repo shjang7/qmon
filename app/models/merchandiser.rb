9de55008076b71b2881db405c0bbb8fb5609a0a0
shjang7 <suhy.jang.shj@gmail.com>
2020-03-04 20:58:21 +0900

    Add merchandiser model

---
+++ app/models/merchandiser.rb
@@ -0,0 +1,7 @@
+class Merchandiser < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :products, inverse_of: :merchandiser, dependent: :destroy
+end
