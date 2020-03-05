da562115088b584c2df8e29d49039ab8ddfe49d0
shjang7 <suhy.jang.shj@gmail.com>
2020-03-05 09:36:22 +0900

    Add rspec for merchandiser model

--- app/models/merchandiser.rb
+++ app/models/merchandiser.rb
@@ -4,4 +4,8 @@ class Merchandiser < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :products, inverse_of: :merchandiser, dependent: :destroy
+  validates :username, :business_name, :business_number, uniqueness: true
+  validates :username, :business_name, :owner_name, :business_number, presence: true
+  validates :contact_number, :company_address, presence: true
+  validates :username, length: { minimum: 4, maximum: 50 }
 end
