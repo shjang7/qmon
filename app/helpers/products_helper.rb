1527184439b2e7ee84ca68f96f2864c967a8d2fd
shjang7 <suhy.jang.shj@gmail.com>
2020-03-08 17:47:18 +0900

    Update product filter by category

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,4 +2,8 @@ module ProductsHelper
   def shipping_fee_choice
     ['₩ 0', '₩ 2500']
   end
+
+  def category_names
+    Category.all.map(&:name)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ require 'faker'
 
 Category.delete_all
 
-['health', 'spring', 'fashion', 'travel', 'beauty', 'food'].each do |c|
+['best', 'health', 'spring', 'fashion', 'travel', 'beauty', 'food'].each do |c|
   Category.create!(name: c)
 end
 
