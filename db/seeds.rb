a2bc15d0173ed23f7ae4e6ad87f9bcdf61649141
shjang7 <lucy.sh.jang@gmail.com>
2020-03-11 18:35:08 +0900

    Update db seed file for user, product without image attachment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,19 @@
 require 'faker'
 
-Category.delete_all
+Merchandiser.destroy_all
+Customer.destroy_all
+Category.destroy_all
 
+categories = []
 ['best', 'health', 'spring', 'fashion', 'travel', 'beauty', 'food'].each do |c|
-  Category.create!(name: c)
+  categories.push Category.create!(name: c)
 end
 
 merchandiser_username = 'darkjoke'
+customer_username = 'sharpsteam'
 common_password = 'foobar'
-Merchandiser.delete_all
 
-Merchandiser.create!(
+m1 = Merchandiser.create(
   username: merchandiser_username,
   password: common_password,
   password_confirmation: common_password,
@@ -20,4 +23,23 @@ Merchandiser.create!(
   contact_number: Faker::PhoneNumber.cell_phone,
   business_number: Faker::Company.swedish_organisation_number,
   company_address: Faker::Address.full_address
+)
+
+categories.each do |c|
+  p = m1.products.build(
+    title: Faker::Company.bs,
+    price: Faker::Number.number(digits: 5),
+    shipping_fee: 0,
+    category_id: c.id
+  )
+  p.save(validate: false)
+end
+
+Customer.create(
+  username: customer_username,
+  name: Faker::Name.name,
+  contact_number: Faker::PhoneNumber.cell_phone,
+  address: Faker::Address.full_address,
+  password: 'foobar',
+  password_confirmation: 'foobar'
 )
