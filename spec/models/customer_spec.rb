9d6070e120015e694a6b645098afa1482e6d47a0
shjang7 <suhy.jang.shj@gmail.com>
2020-03-12 20:01:11 +0900

    Add customer test, update merchandiser test for easier reading

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  context 'has correct informations' do
+    it 'with valid attributes' do
+      password = Faker::Alphanumeric.alpha(number: 6)
+      jen = Customer.new(
+        username: Faker::Name.unique.name,
+        password: password,
+        password_confirmation: password,
+        contact_number: Faker::PhoneNumber.cell_phone,
+        address: Faker::Address.full_address
+      )
+    end
+
+    it 'with a right factory' do
+      expect(build(:customer)).to be_valid
+    end
+  end
+
+  context 'cannot be valid' do
+    it 'without a username' do
+      m = build(:customer, username: '')
+      expect(m).to_not be_valid
+    end
+
+    it 'without a contact_number' do
+      m = build(:customer, contact_number: '')
+      expect(m).to_not be_valid
+    end
+
+    it 'without a address' do
+      m = build(:customer, address: '')
+      expect(m).to_not be_valid
+    end
+
+    it 'without a password' do
+      m = build(:customer, password: '')
+      expect(m).to_not be_valid
+    end
+  end
+
+  context 'cannot be valid' do
+    it 'with duplicated username' do
+      mos = create(:customer)
+      jen = build(:customer, username: mos.username)
+      expect(jen).to_not be_valid
+    end
+
+    it 'with a short username under 4 characters' do
+      jen = build(:customer, username: Faker::Alphanumeric.alphanumeric(number: 3))
+      expect(jen).to_not be_valid
+    end
+
+    it 'with a long username over 50 characters' do
+      jen = build(:customer, username: Faker::Alphanumeric.alphanumeric(number: 51))
+      expect(jen).to_not be_valid
+    end
+  end
 end
--- spec/models/merchandiser_spec.rb
+++ spec/models/merchandiser_spec.rb
@@ -1,100 +1,101 @@
 require 'rails_helper'
 
 RSpec.describe Merchandiser, type: :model do
-  context 'with valid attributes' do
-    it 'has correct informations' do
+  context 'has correct informations' do
+    it 'with valid attributes' do
+      password = Faker::Alphanumeric.alpha(number: 6)
       jen = Merchandiser.new(
-        username: 'sample-user-0',
-        email: 'sample-user-0@example.org',
-        password: 'foobar',
-        password_confirmation: 'foobar',
-        business_name: 'sample-company-0',
-        owner_name: 'sample owner',
-        contact_number: '(02)01234' ,
-        business_number: '5678',
-        company_address: 'sample address'
+        username: Faker::Name.unique.name,
+        email: Faker::Internet.email,
+        password: password,
+        password_confirmation: password,
+        business_name: Faker::Name.unique.name,
+        owner_name: Faker::Name.name,
+        contact_number: Faker::PhoneNumber.cell_phone,
+        business_number: Faker::Company.unique.swedish_organisation_number,
+        company_address: Faker::Address.full_address
       )
       expect(jen).to be_valid
     end
 
-    it 'has a right factory' do
+    it 'with a right factory' do
       expect(build(:merchandiser)).to be_valid
     end
   end
 
-  context 'with invalid attributes for blank' do
-    it 'cannot have a blank username' do
+  context 'cannot be valid' do
+    it 'without a username' do
       m = build(:merchandiser, username: '')
       expect(m).to_not be_valid
     end
 
-    it 'cannot have a blank business_name' do
-      m = build(:merchandiser, business_name: '')
-      expect(m).to_not be_valid
-    end
-
-    it 'cannot have a blank owner_name' do
-      m = build(:merchandiser, owner_name: '')
-      expect(m).to_not be_valid
-    end
-
-    it 'cannot have a blank business_number' do
-      m = build(:merchandiser, business_number: '')
-      expect(m).to_not be_valid
-    end
-
-    it 'cannot have a blank contact_number' do
-      m = build(:merchandiser, contact_number: '')
-      expect(m).to_not be_valid
-    end
-
-    it 'cannot have a blank company_address' do
-      m = build(:merchandiser, company_address: '')
-      expect(m).to_not be_valid
-    end
-
-    it 'cannot have a blank email' do
+    it 'without a email' do
       m = build(:merchandiser, email: '')
       expect(m).to_not be_valid
     end
 
-    it 'cannot have a blank password' do
+    it 'without a business_name' do
+      m = build(:merchandiser, business_name: '')
+      expect(m).to_not be_valid
+    end
+
+    it 'without a owner_name' do
+      m = build(:merchandiser, owner_name: '')
+      expect(m).to_not be_valid
+    end
+
+    it 'without a business_number' do
+      m = build(:merchandiser, business_number: '')
+      expect(m).to_not be_valid
+    end
+
+    it 'without a contact_number' do
+      m = build(:merchandiser, contact_number: '')
+      expect(m).to_not be_valid
+    end
+
+    it 'without a company_address' do
+      m = build(:merchandiser, company_address: '')
+      expect(m).to_not be_valid
+    end
+
+    it 'without a password' do
       m = build(:merchandiser, password: '')
       expect(m).to_not be_valid
     end
   end
 
-  context 'with invalid attributes for wrong info' do
-    it 'cannot be valid with duplicate username' do
+  context 'cannot be valid' do
+    it 'with duplicated username' do
       mos = create(:merchandiser)
       jen = build(:merchandiser, username: mos.username)
       expect(jen).to_not be_valid
     end
 
-    it 'cannot be valid with duplicate email' do
+    it 'with duplicated email' do
       mos = create(:merchandiser)
       jen = build(:merchandiser, email: mos.email)
       expect(jen).to_not be_valid
     end
 
-    it 'cannot be valid with duplicate business_name' do
+    it 'with duplicated business_name' do
       mos = create(:merchandiser)
       jen = build(:merchandiser, business_name: mos.business_name)
       expect(jen).to_not be_valid
     end
 
-    it 'cannot be valid with duplicate business_number' do
+    it 'with duplicated business_number' do
       mos = create(:merchandiser)
       jen = build(:merchandiser, business_number: mos.business_number)
       expect(jen).to_not be_valid
     end
 
-    it 'cannot be valid with a short username under 4 characters' do
+    it 'with a short username under 4 characters' do
       jen = build(:merchandiser, username: Faker::Alphanumeric.alphanumeric(number: 3))
       expect(jen).to_not be_valid
     end
 
-    it 'cannot be valid with a long username over 50 characters' do
+    it 'with a long username over 50 characters' do
       jen = build(:merchandiser, username: Faker::Alphanumeric.alphanumeric(number: 51))
       expect(jen).to_not be_valid
     end
