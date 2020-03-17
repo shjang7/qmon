a7b72ba0a9d92b11e537e4643f27a745135760c0
shjang7 <suhy.jang.shj@gmail.com>
2020-03-17 17:40:07 +0900

    Add purchase, order controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :customers
   devise_for :merchandisers
   resources :products
+  resources :purchases
+  resources :orders
   get 'static_pages/signup'
 
   root 'products#index'
