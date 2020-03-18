7a03983a91c27544e6986eddb1fa67720b66539d
shjang7 <suhy.jang.shj@gmail.com>
2020-03-18 15:35:40 +0900

    Update database to align text

--- db/migrate/20200316221351_create_purchases.rb
+++ db/migrate/20200316221351_create_purchases.rb
@@ -1,7 +1,7 @@
 class CreatePurchases < ActiveRecord::Migration[6.0]
   def change
     create_table :purchases do |t|
-      t.string    :purchase_number,    null: false, default: ""
+      t.string    :purchase_number,   null: false, default: ""
       t.integer   :price,             null: false, default: 0
       t.integer   :shipping_fee,      null: false, default: 0
       t.string    :recipient_name,    null: false, default: ""
@@ -10,7 +10,7 @@ class CreatePurchases < ActiveRecord::Migration[6.0]
       t.integer   :shipping_status,   default: 0
       t.datetime  :arrive_date
       t.boolean   :confirmed,         default: false
-      t.belongs_to :customer,       null: false, foreign_key: true
+      t.belongs_to :customer,         null: false, foreign_key: true
 
       t.timestamps
     end
