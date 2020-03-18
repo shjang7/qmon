00d46f1f116f0253ba5bac59176150acbc18f7f4
shjang7 <lucy.sh.jang@gmail.com>
2020-03-18 14:00:12 +0900

    Update to move shipping info into purchase db

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,5 +1,16 @@
 class Purchase < ApplicationRecord
   belongs_to  :customer,  inverse_of: :purchases
-  has_many    :orders,    inverse_of: :purchase
+  has_many    :orders,    inverse_of: :purchase, dependent: :destroy
   accepts_nested_attributes_for :orders, reject_if: :all_blank, allow_destroy: true
+
+  validates :purchase_number, :price, :shipping_fee, presence: true
+  validates :recipient_name, :recipient_contact, :recipient_address, presence: true
+  validates :confirmed, :customer_id, :shipping_status, presence: true
+  validates :confirmed, inclusion: { in: [true] }
+
+  class << self
+    def unconfirmed_destroy(customer_id)
+      where(customer_id: customer_id).where(confirmed: false).destroy_all
+    end
+  end
 end
--- db/migrate/20200316221351_create_purchases.rb
+++ db/migrate/20200316221351_create_purchases.rb
@@ -1,13 +1,15 @@
 class CreatePurchases < ActiveRecord::Migration[6.0]
   def change
     create_table :purchases do |t|
-      t.integer :purchase_number
-      t.integer :price,             null: false, default: 0
-      t.integer :shipping_fee,      null: false, default: 0
-      t.string  :recipient_name,     null: false, default: ""
-      t.string  :recipient_contact,  null: false, default: ""
-      t.string  :recipient_address,  null: false, default: ""
-      t.boolean :confirmed,         default: false
+      t.string    :purchase_number,    null: false, default: ""
+      t.integer   :price,             null: false, default: 0
+      t.integer   :shipping_fee,      null: false, default: 0
+      t.string    :recipient_name,    null: false, default: ""
+      t.string    :recipient_contact, null: false, default: ""
+      t.string    :recipient_address, null: false, default: ""
+      t.integer   :shipping_status,   default: 0
+      t.datetime  :arrive_date
+      t.boolean   :confirmed,         default: false
       t.belongs_to :customer,       null: false, foreign_key: true
 
       t.timestamps
