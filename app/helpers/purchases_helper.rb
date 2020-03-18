3ac1c781378eddd3d1409209e0168d656949dd4d
shjang7 <suhy.jang.shj@gmail.com>
2020-03-18 15:28:35 +0900

    Add purchase view, complete order message page

--- app/helpers/purchases_helper.rb
+++ app/helpers/purchases_helper.rb
@@ -7,4 +7,15 @@ module PurchasesHelper
       "#{items.first.title} and #{items.size - 1}"
     end
   end
+
+  def render_shipping_status(shipping_status, arrive_date)
+  case shipping_status
+  when 0
+    'Preparing product'
+  when 1
+    'Shipment in progress'
+  when 2
+    "Shipment completed on #{arrive_date}"
+  end
+  end
 end
