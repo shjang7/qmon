c1f7b62d17cc6fcfb218998172a274b69d188e4e
shjang7 <suhy.jang.shj@gmail.com>
2020-03-21 23:30:55 +0900

    Improve faker random speed, Add helper test

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :order do
-    title { Faker::Alphanumeric.alphanumeric(number: 6) }
-    quantity { Faker::Number.number(digits: 3) }
-    price { Faker::Number.number(digits: 5) }
+    title { Faker::Alphanumeric.alphanumeric(number: 2) }
+    quantity { Faker::Number.number(digits: 2) }
+    price { Faker::Number.number(digits: 2) }
     purchase
   end
 end
--- spec/factories/purchases.rb
+++ spec/factories/purchases.rb
@@ -1,13 +1,12 @@
 FactoryBot.define do
   factory :purchase do
-    purchase_number { Faker::Number.number(digits: 4) }
-    price { Faker::Number.number(digits: 3) }
+    purchase_number { Faker::Number.number(digits: 2) }
+    price { Faker::Number.number(digits: 2) }
     shipping_fee { 0 }
-    recipient_name { Faker::Alphanumeric.alphanumeric(number: 6) }
+    recipient_name { Faker::Alphanumeric.alphanumeric(number: 2) }
     recipient_contact { Faker::PhoneNumber.cell_phone }
     recipient_address { Faker::Address.full_address }
     shipping_status { 1 }
-    arrive_date { "" }
     confirmed { true }
     customer
   end
